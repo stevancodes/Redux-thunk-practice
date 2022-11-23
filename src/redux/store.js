6091015a8a25eabef6188ee02c62c66611e3fc68
stevancodes <stevan.code@gmail.com>
2022-11-23 12:33:54 +0100

    Middleware Logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import thunk from "redux-thunk";
 import postReducer from "./reducer";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: postReducer,
-  middleware: [thunk],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk).concat(logger),
 });
 
 export default store;
